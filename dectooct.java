49e779232b74a0fdb8c841ea276ff1caee0e11af
priyankamishra123 <priyanka.itengineer@gmail.com>
2018-08-28 11:21:54 -0700

    Add files via upload

---
+++ dectooct.java
@@ -0,0 +1,11 @@
+public class dectooct {
+
+	public static void main(String... args) {
+		// TODO Auto-generated method stub
+		int count=0;
+		count++;
+		if(count==1)
+			main("sri");
+		System.out.println("main"+args.length);
+}
+}
